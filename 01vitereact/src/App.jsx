15ff927b38c126e459727265fb24c17cde340ccb
Asadullah Khan <asadullahkhan3096@gmail.com>
2026-03-12 23:29:54 +0530

    created own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,12 @@ import Chai from "./chai"
 
 function App() {
 
+  const username = 'Khan Coding'
   return (
     <>
     <Chai/>
+    <h1>Chai aur react {username}</h1> 
+    <p>test para</p>
     </>
   )
 }
---
+++ customReact/customereact.js
@@ -0,0 +1,22 @@
+
+function customeRender(reactElement,container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.Children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue
+        domElement.setAttribute(prop,reactElement.props[prop]) 
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https/google.com',
+        target: '_blank'
+    },
+    Children : 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root');
+customeRender(reactElement,mainContainer)
